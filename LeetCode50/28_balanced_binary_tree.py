4966bb1f626a4b147730da0c6c2c0c17fc0b2e26
Xiaochi (George) Li <343928729@qq.com>
2019-01-24 23:36:04 -0500

    28

---
+++ LeetCode50/28_balanced_binary_tree.py
@@ -0,0 +1,60 @@
+from binary_tree import TreeNode
+from unittest import TestCase
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        pass
+
+
+class BruteForceTopDownRecursion(Solution):
+    """Time O(n^2) because max depth is recalculated every time
+    Space O(n)"""
+    def max_depth(self, root: TreeNode):
+        if root is None:
+            return 0
+        return max(self.max_depth(root.left), self.max_depth(root.right)) + 1
+
+    def isBalanced(self, root):
+        if root is None:
+            return True
+        if abs(self.max_depth(root.left) - self.max_depth(root.right)) <= 1 and self.isBalanced(root.left)\
+                and self.isBalanced(root.right):
+            return True
+        else:
+            return False
+
+
+class BottomUp(Solution):
+    def isBalanced(self, root):
+        pass
+
+
+class Test(TestCase):
+    def test_true(self):
+        t = TreeNode(3)
+        t.left = TreeNode(9)
+        t.right = TreeNode(20)
+        t.right.left = TreeNode(15)
+        t.right.left = TreeNode(7)
+        self.assertTrue(BruteForceTopDownRecursion().isBalanced(t))
+
+    def test_false(self):
+        t = TreeNode(1)
+        t.left = TreeNode(2)
+        t.right = TreeNode(2)
+        t.left.left = TreeNode(3)
+        t.left.right = TreeNode(3)
+        t.left.left.left = TreeNode(4)
+        t.left.left.right = TreeNode(4)
+        self.assertFalse(BruteForceTopDownRecursion().isBalanced(t))
